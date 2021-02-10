b64bca30c59479bc07bc6b5446486d672d08319f
Hoybaby <mbartek436@gmail.com>
2021-02-10 13:57:29 -0500

    added some js to made the functionalty with a website

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -1,5 +1,13 @@
 const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link')
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
+})
+
+//creating a loop to happen for each link in the hamburger section"
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open'); //this will remove a class with the classList.remove so when clicked it will remove the option bar
+    })
 })
